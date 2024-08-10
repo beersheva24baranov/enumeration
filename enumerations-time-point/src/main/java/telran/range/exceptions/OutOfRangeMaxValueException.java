e0bee629f4abb7835b34b5eacf0c13dbe8398a36
AlexanderBaranov <avbc@mail.ru>
2024-08-10 21:05:27 +0300

    remove wrong files

--- enumerations-time-point/src/main/java/telran/range/exceptions/OutOfRangeMaxValueException.java
+++
@@ -1,7 +0,0 @@
-package telran.range.exceptions;
-
-public class OutOfRangeMaxValueException extends Exception{
-    public OutOfRangeMaxValueException(int max, int value) {
-        super(String.format("Out of Range max: %d, number: %d", max, value));
-    }
-}
--- enumerations-time-point/src/main/java/telran/range/exceptions/OutOfRangeMinValueException.java
+++
@@ -1,7 +0,0 @@
-package telran.range.exceptions;
-
-public class OutOfRangeMinValueException extends Exception{
-    public OutOfRangeMinValueException(int min, int number) {
-        super(String.format("Out of Range min: %d, number: %d", min, number));
-    }
-}
